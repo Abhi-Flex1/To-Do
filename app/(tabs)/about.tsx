c2de6ba287d3ca06d97c1989df152a97470aa5ca
Abhi-Flex1 <abhi_flex@icloud.com>
2026-02-13 22:16:56 +0100

    Basic App functionality added

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, Button, StyleSheet } from 'react-native';
 export default function About() {
     return(
         <View style={style.container}>
-               <Text>Helo</Text>
+               <Text style={style.text} >This is my first React Native app.</Text>
         </View>
     );
 }
@@ -11,6 +11,12 @@ export default function About() {
 const style = StyleSheet.create ({
     container: {
         flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: "#FFFFFF",
+    },
+
+    text: {
+        fontSize: 20,
     }
 });
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,35 @@
-import { Text, View, StyleSheet, FlatList, Pressable } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Pressable, Modal, Button, TextInput } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useState } from 'react';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 type Task = {
     id: string;
     text: string;
-    date: string;
+    due: Date;
     completed: boolean;
 }
 
 export default function Home() {
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [draftText, setDraftText] = useState("");
+    const [pickerVisible, setPickerVisible] = useState(false);
+    const [draftDate, setDraftDate] = useState(new Date()); 
 
-    const addTask = () => {
-        const newTask: Task = {
+    const saveTask = () => {
+        if (draftText.trim() === "") return;
+
+        const newTask = {
             id: Date.now().toString(),
-            text: "Task",
-            date: Date.now().toString(),
+            text: draftText,
+            due: draftDate,
             completed: false,
         };
-        setTasks((prev) => [newTask, ...prev]);
-    };  
+        setTasks(prev => [newTask, ...prev]);
+        setDraftText("");
+        setModalVisible(false);
+    };
 
     const toggleTask = (id: string) => {
         setTasks((prev) =>
@@ -47,11 +56,40 @@ export default function Home() {
                                 <Ionicons name={item.completed ? "checkbox" : "square-outline"} size={32}/>
                             </Pressable>
                             <Text>    {item.text}</Text>
+                            <Pressable onPress={() => deleteTask(item.id)} style={{marginLeft: 'auto',}}>
+                                <Ionicons name='trash-bin' size={24}/>
+                            </Pressable>
                         </View>
                     )}/>
-               <Pressable style={style.create} onPress={addTask}>
+               <Pressable style={style.create} onPress={() => setModalVisible(true)}>
                     <Ionicons name={'add'} color="#FFFFFF" size={24} />
                </Pressable>
+
+               <Modal visible={modalVisible}>
+                    <View style={style.modal}>
+                        <TextInput 
+                            style={style.input}
+                            placeholder='Task'
+                            value={draftText}
+                            onChangeText={setDraftText}
+                            placeholderTextColor="#5d5d5d"
+                        />
+                        <Text style={style.button} onPress={() => setPickerVisible(true)}>Pick Date and Time</Text>
+                        <DateTimePickerModal 
+                            isVisible={pickerVisible}
+                            mode='datetime'
+                            date={draftDate}
+                            onConfirm={(date) => {
+                                setDraftDate(date);
+                                setPickerVisible(false);
+                            }}
+                            onCancel={() => setPickerVisible(false)}
+                        />
+                        <Text>{draftDate.toString()}</Text>
+                        <Text style={style.button} onPress={saveTask}>Save</Text>
+                    </View>
+                    
+               </Modal>
         </View>
     );
 }
@@ -84,5 +122,30 @@ const style = StyleSheet.create({
         marginBottom: 10,
         borderRadius: 10,
         backgroundColor: '#F2F2F2',
+    },
+
+    modal: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    button: {
+        color: '#FFFFFF',
+        backgroundColor: "#0063FF",
+        margin: 15,
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderRadius: 20,
+    },
+
+    input: {
+        margin: 15,
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#cecece',
+        borderRadius: 20,
     },
 });
