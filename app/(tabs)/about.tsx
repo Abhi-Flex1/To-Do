c2e8d7bce213673a56553259d17e5d83e8a3a016
Abhi-Flex1 <abhi_flex@icloud.com>
2026-02-13 23:53:28 +0100

    Added better About section

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,13 @@
 import { Text, View, Button, StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
+import { withLayoutContext } from 'expo-router';
 
 export default function About() {
     return(
         <View style={style.container}>
-               <Text style={style.text} >This is my first React Native app.</Text>
+                <Image source={require("../../assets/images/Frame.png")} style={style.image} />
+                <Text style={style.text} >To-Do</Text>
+                <Text style={style.subtext} >Simple To-Do app made by Abhi</Text>
         </View>
     );
 }
@@ -17,6 +21,20 @@ const style = StyleSheet.create ({
     },
 
     text: {
-        fontSize: 20,
+        fontSize: 25,
+        marginBottom: 10,
+    },
+
+    subtext: {
+        fontSize: 18,
+    },
+
+    image: {
+        borderRadius: 20,
+        borderColor: '#969696',
+        borderWidth: 2,
+        height: 100,
+        width: 100,
+        marginBottom: 20,
     }
 });
